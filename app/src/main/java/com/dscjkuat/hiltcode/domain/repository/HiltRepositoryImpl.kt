dde76aa9e937acffaf21f26b688cc06507e8c243
OtienoSamwel <otienosamwel135@gmail.com>
2021-06-25 10:23:12 +0300

    implement members from the repository interface

--- app/src/main/java/com/dscjkuat/hiltcode/domain/repository/HiltRepositoryImpl.kt
+++ app/src/main/java/com/dscjkuat/hiltcode/domain/repository/HiltRepositoryImpl.kt
@@ -1,17 +1,30 @@
 package com.dscjkuat.hiltcode.domain.repository
 
+import com.dscjkuat.hiltcode.data.local.HiltDao
 import com.dscjkuat.hiltcode.data.remote.ApiEndpoints
-import com.dscjkuat.hiltcode.domain.models.PostResponse
-import com.dscjkuat.hiltcode.domain.models.UsersResponse
+import com.dscjkuat.hiltcode.domain.models.Post
+import com.dscjkuat.hiltcode.domain.models.User
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class HiltRepositoryImpl @Inject constructor(val api: ApiEndpoints): HiltRepository {
+class HiltRepositoryImpl @Inject constructor(
+    private val dao: HiltDao,
+    private val api: ApiEndpoints,
+) : HiltRepository {
+    override val users: Flow<List<User>>
+        get() = dao.getUsers()
 
-    override suspend fun getUsers(limit: Int): UsersResponse {
-        return api.getUser(limit)
+    override val posts: Flow<List<Post>>
+        get() = dao.getPosts()
+
+    override suspend fun getUsers(limit: Int) {
+
+        val result = api.getUser(limit)
+        dao.updateUsers(result.data)
     }
 
-    override suspend fun getUsersPosts(id: String, limit: Int): PostResponse {
-        return api.getUserPosts(id, limit)
+    override suspend fun getUsersPosts(id: String, limit: Int) {
+        val result = api.getUserPosts(id, limit)
+        dao.updatePost(result.data)
     }
 }
