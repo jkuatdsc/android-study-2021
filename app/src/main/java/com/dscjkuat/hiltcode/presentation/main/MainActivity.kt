7afc394b0e1751553d07a9bf575979894ed19d89
OtienoSamwel <otienosamwel135@gmail.com>
2021-06-25 10:24:04 +0300

    collect user data from our flow

--- app/src/main/java/com/dscjkuat/hiltcode/presentation/main/MainActivity.kt
+++ app/src/main/java/com/dscjkuat/hiltcode/presentation/main/MainActivity.kt
@@ -2,9 +2,13 @@ package com.dscjkuat.hiltcode.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.dscjkuat.hiltcode.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -16,5 +20,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewModel.getUsers()
+        lifecycleScope.launch {
+            viewModel.users.collect {
+                Log.d("MainActivity", "onCreate: $it")
+            }
+        }
     }
 }
