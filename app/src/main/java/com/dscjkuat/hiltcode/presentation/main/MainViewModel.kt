c3e8ab4c19d2e585ff4611aaf3217fb571427d51
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-15 23:28:57 +0300

    - import dependency

--- app/src/main/java/com/dscjkuat/hiltcode/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/dscjkuat/hiltcode/presentation/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.dscjkuat.hiltcode.presentation.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dscjkuat.hiltcode.domain.repository.HiltRepository
